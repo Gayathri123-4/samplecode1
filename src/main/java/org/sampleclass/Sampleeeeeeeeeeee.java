9cd03df6be5275abbd130485416329651496c3d0
Gayathri123-4 <gayurey0606@gmail.com>
2022-03-14 10:13:29 +0530

    code1

---
+++ src/main/java/org/sampleclass/Sampleeeeeeeeeeee.java
@@ -0,0 +1,29 @@
+package org.sampleclass;
+
+import org.testng.annotations.Test;
+
+public class Sampleeeeeeeeeeee {
+	
+@Test
+private void test1() {
+	System.out.println("testA");
+
+}
+	@Test
+	private void test2() {
+		System.out.println("testB"); 
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
